73c90a66637646f580b219aa9f3f87099ff0580f
Marieli Mariano Goncalves Dias <mmariano@c1r10p4.42sp.org.br>
2024-11-04 18:47:01 -0300

    Fix memset

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marielidias <marielidias@student.42.fr>    +#+  +:+       +#+        */
+/*   By: mmariano <mmariano@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 18:46:34 by mmariano          #+#    #+#             */
-/*   Updated: 2024/10/29 18:54:58 by marielidias      ###   ########.fr       */
+/*   Updated: 2024/11/04 18:23:40 by mmariano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@ void	*ft_memset(void *b, int c, size_t len)
 	tmp = (unsigned char *)b;
 	while (len > 0)
 	{
-		*(tmp++) = (unsigned char)c;
+		*tmp = (unsigned char)c;
+		tmp++;
 		len--;
 	}
 	return (b);
