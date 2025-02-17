7d24a4c7f98c102ae123543eb88982baa01550d6
Marieli Mariano Goncalves Dias <mmariano@c3r5p4.42sp.org.br>
2025-02-17 14:13:57 -0300

    Add bonus functions

---
+++ ft_lstclear.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmariano <mmariano@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/03 15:15:34 by marieli           #+#    #+#             */
+/*   Updated: 2025/02/17 14:10:16 by mmariano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*temp;
+
+	while (*lst)
+	{
+		temp = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = temp;
+	}
+	*lst = NULL;
+}
---
+++ ft_lstsize.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstsize.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmariano <mmariano@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/03 15:16:13 by marieli           #+#    #+#             */
+/*   Updated: 2025/02/17 14:07:14 by mmariano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/libft.h"
+
+int	ft_lstsize(t_list *lst)
+{
+	int	node_count;
+
+	node_count = 0;
+	while (lst != NULL)
+	{
+		node_count++;
+		lst = lst->next;
+	}
+	return (node_count);
+}
